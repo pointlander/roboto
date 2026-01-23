11467f7b12a96904a818586d07324982d1153448
Andrew Snodgrass <pointlander@gmail.com>
2026-01-23 10:14:25 -0600

    Added platform dependent constants file

---
+++ constants_wasm.go
@@ -0,0 +1,10 @@
+// Copyright 2026 The Roboto Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build wasm
+// +build wasm
+
+package main
+
+const Iterations = 4
